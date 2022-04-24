a5c4e9bacd186195f7e58fd507de74d11ee73bb2
Newbie_dqt <84654263+noob-dqt@users.noreply.github.com>
2022-04-24 19:55:16 +0800

    Add files via upload

---
+++ Apriori.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5+5;
+//本数据集中最大数字是16469
+//使用vector存储单个项集
+int r,mx; //总共有r行数据，数据集中最大的数字是mx
+int cnt[N];     //统计一项集
+int surp;       //支持度
+vector<vector<int>>res;    //结果集，一个vector是一个项集
+bitset<N>sear,its;            //扫描数据库时用于存放一行数据状态，its存放项集
+string s;
+set<vector<int>>L,C;
+ifstream fin("./C++/retail.dat");
+void getnum(){
+    int x=0;
+    for(char c:s){
+        if(c>='0'&&c<='9'){
+            x=x*10+c-'0';    
+        }
+        else{
+            cnt[x]++;
+            mx=max(mx,x);
+            x=0; 
+        }
+    }
+    if(s.back()!=' '){
+        cnt[x]++;
+        mx=max(mx,x);
+    }
+}
+void read(){
+    if(!fin.is_open()){
+        cout<<"there is no such file!\n";
+        system("pause");
+        exit(0);
+    }
+    while (getline(fin,s))
+    {
+        getnum();
+        r++;
+    }
+}
+bool isvaild(vector<int>a,vector<int>b){    //ab长度为n，则ab前n-1个数字必须相同才能合并
+    int n=a.size();
+    for(int i=0;i<n-1;i++){
+        if(a[i]!=b[i]) return false;
+    }
+    return true;
+}
+bool check(vector<int>a){       //检查m项集(a)的所有m-1项集中是否都存在于Li-1中
+    //m-项集中包含m个m-1项集
+    vector<int>b;
+    int n=a.size();
+    for(int i=0;i<n;i++){
+        b.clear();
+        for(int j=0;j<n;j++){
+            if(j==i) continue;
+            b.push_back(a[j]);
+        }
+        if(L.find(b)==L.end()) return false;    //不符合条件的项集舍去
+    }
+    return true;
+}
+bool iscount(vector<int>a){         //扫描数据库，检查a是否是频繁项集
+    its.reset();
+    for(int num:a) its[num]=1;
+    int cnt=0,x;
+    fin.clear();
+    fin.seekg(0);
+    while (getline(fin,s))
+    {
+        sear.reset();
+        x=0;
+        for(char c:s){
+            if(c>='0'&&c<='9'){
+                x=x*10+c-'0';
+            }
+            else{
+                sear[x]=1;
+                x=0;
+            }
+        }
+        if(s.back()!=' '){
+            sear[x]=1;
+        }
+        if((sear&its)==its) cnt++;
+    }
+    return cnt>=surp;
+}
+int main()
+{
+    read();     //读取数据
+    cout<<"please input the minimum support:"<<endl;
+    double zp;
+    cin>>zp;
+    surp=ceil(r*zp);
+    //先处理出L1，从小到大遍历整理成有序的
+    for(int i=0;i<=mx;i++){
+        if(cnt[i]>=surp){
+            L.insert({i});
+        }
+    }
+    vector<int>tp;          //产生临时项集
+    while (L.size()>1)      //等于1时不会再产生新的频繁项集
+    {
+        C.clear();          //清空Ci-1的项集
+        for(auto tp:L) res.push_back(tp);  //L中结果放入结果集
+        //Li生成新的Ci+1集合
+        for(auto i=L.begin();i!=L.end();i++){
+            auto j=i;j++;
+            for(;j!=L.end();j++){
+                if(!isvaild(*i,*j)) break;  //剪枝，数字有序后面的都不可能产生频繁项集
+                //否则将a、b合并，只用将a、b前n-1个数取出，再将a,b末尾分别插入新项集
+                tp.clear();
+                for(int k=0;k<(*i).size();k++) tp.push_back((*i)[k]);
+                tp.push_back((*j).back());
+                //检查新生成的m项集tp的所有m-1项集是否存在于Li中，若不存在则抛弃，若符合条件则放入Ci+1中
+                if(!check(tp)) continue;
+                C.insert(tp);
+            }
+        }
+        //Ci+1中筛选出Li+1
+        //扫描数据库，并统计每一项集的置信度
+        L.clear();      //清空L，空间重复利用，将新产生的频繁项集存入L
+        for(auto tp:C){
+            if(iscount(tp)) L.insert(tp);
+        }
+        if(L.size()==1) res.push_back(*L.begin());     //特殊处理最后一项
+    }
+    cout<<"total: "<<res.size()<<" items\n";
+    for(auto tp:res){
+        printf("{ ");
+        for(int i:tp) printf("%d ",i);
+        printf("}\n");
+
+    }
+    fin.close();
+    return 0;
+}
---
+++ FPGrowth.cpp
@@ -0,0 +1,255 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+5;
+struct fpnode
+{
+    int item;       //项
+    int count;      //计数
+    fpnode *fa;     //父节点
+    map<int,fpnode*>sons;   //子孙节点
+    fpnode *next;   //指向item下一个位置
+    fpnode(int x,int y){
+        sons.clear();
+        item=x;
+        count=y;
+        fa=nullptr;
+        next=nullptr;
+    }
+    fpnode(){
+        sons.clear();
+        item=-1;
+        count=0;
+        fa=nullptr;
+        next=nullptr;
+    }
+};
+struct node         //头表节点
+{
+    int item;
+    int count;
+    fpnode * head;
+    node(){
+        head=nullptr;
+        count=0;
+        item=-1;
+    }
+    node(int x,int y){
+        head=nullptr;
+        item=x;
+        count=y;
+    }
+    bool operator<(const node t)const{
+        if(count==t.count) return item<t.item;
+        return count>t.count;
+    }
+};
+class FP_Tree
+{
+public:
+    bool flag;              //标记树是否是单分支树,true为单分支
+    fpnode *root;           //FP树根
+    vector<node>Header_Table;
+    FP_Tree(){
+        root=new fpnode(-1,0);      //-1标识根节点
+        flag=1;
+        Header_Table.clear();
+    }
+};
+string path,s;        //path存放路径，s存放读取的单行数据
+int col,minsup;         //col数据总行数，minsup最小支持度
+map<int,fpnode*>mp;     //建树工具
+unordered_map<int,int>cnt;
+vector<vector<int>>res;
+map<int,int>ans;
+vector<vector<int>>db;  //条件模式基
+vector<int>alpha;
+vector<int>Beta;
+vector<int>tmp;         //建树存每条事务
+bool cmp(int x,int y){
+    if(cnt[x]==cnt[y]) return x<y;
+    return cnt[x]>cnt[y];
+}
+FP_Tree first_construct(){
+    ifstream fin(path);
+    if(!fin.is_open()){
+        cout<<"there is no such file!\n";
+        system("pause");
+        exit(-1);
+    }
+    cnt.clear();
+    while (getline(fin,s)){
+        if(s==" "||s.empty()) continue;
+        int x=0;
+        for(char c:s){
+            if(c>='0'&&c<='9') x=x*10+c-'0';
+            else{
+                cnt[x]++;
+                x=0;
+            }
+        }
+        if(s.back()!=' ') cnt[x]++;
+        col++;
+    }
+    double sur;
+    cout<<"please input the minimum support(0-1):\n";
+    cin>>sur;
+    if(sur<0||sur>1) {
+        cout<<"wrong support input";
+        system("pause");
+        exit(-1);
+    }
+    minsup=ceil(sur*col);     //计算支持度
+    fin.clear();
+    fin.seekg(0);
+    FP_Tree tree;
+    for(auto v:cnt)
+        if(v.second>=minsup) tree.Header_Table.push_back(node(v.first,v.second));
+    //第二次扫描建树
+    sort(tree.Header_Table.begin(),tree.Header_Table.end());
+    fpnode* p;
+    while (getline(fin,s)){
+        tmp.clear();
+        p=tree.root;
+        if(s==" "||s.empty()) continue;
+        int x=0;
+        for(char c:s){
+            if(c>='0'&&c<='9') x=x*10+c-'0';
+            else{
+                if(cnt[x]>=minsup) tmp.push_back(x);
+                x=0;
+            }
+        }
+        if(s.back()!=' ') if(cnt[x]>=minsup) tmp.push_back(x);
+        sort(tmp.begin(),tmp.end(),cmp);
+        for(int x:tmp){
+            if(p->sons.count(x)){   //已经存在
+                p=p->sons[x];
+                p->count++;
+            }
+            else{
+                fpnode *newnd=new fpnode(x,1);
+                if(!mp.count(x)){
+                    for(auto &v:tree.Header_Table){
+                        if(v.item==x){
+                            v.head=newnd;
+                            break;
+                        }
+                    }
+                }
+                else mp[x]->next=newnd;
+                mp[x]=newnd;
+                newnd->fa=p;
+                p->sons[x]=newnd;
+                if(tree.flag&&p->sons.size()>1) tree.flag=0;
+                p=newnd;
+            }
+        }
+    }
+    return tree;
+}
+FP_Tree build(){    //利用db建树
+    cnt.clear();
+    mp.clear();
+    for(auto &v:db)
+        for(int i=0;i<v.size()-1;i++)
+            cnt[v[i]]+=v.back();
+    FP_Tree tree;
+    for(auto &v:cnt)
+        if(v.second>=minsup) tree.Header_Table.push_back(node(v.first,v.second));
+    sort(tree.Header_Table.begin(),tree.Header_Table.end());
+    fpnode* p;
+    for(auto &v:db){
+        tmp.clear();
+        p=tree.root;
+        for(int i=0;i<v.size()-1;i++) if(cnt[v[i]]>=minsup) tmp.push_back(v[i]);//最后一位是次数不能参与建树
+        sort(tmp.begin(),tmp.end(),cmp);
+        for(int x:tmp){
+            if(p->sons.count(x)){   //已经存在
+                p=p->sons[x];
+                p->count+=v.back();
+            }
+            else{
+                fpnode *newnd=new fpnode(x,v.back());
+                if(!mp.count(x)){
+                    for(auto &v:tree.Header_Table){
+                        if(v.item==x){
+                            v.head=newnd;
+                            break;
+                        }
+                    }
+                }
+                else mp[x]->next=newnd;
+                mp[x]=newnd;
+                newnd->fa=p;
+                p->sons[x]=newnd;
+                if(tree.flag&&p->sons.size()>1) tree.flag=0;
+                p=newnd;
+            }
+        }
+    }
+    return tree;
+}
+void FPgrowth(FP_Tree &tree){
+    if(tree.flag){
+        int len=tree.Header_Table.size();
+        for(int i=1;i<(1<<len);i++)     //二进制枚举所有组合
+        {
+            Beta=alpha;
+            for(int j=0;j<len;j++) if((1<<j)&i) Beta.push_back(tree.Header_Table[j].item);
+            res.push_back(Beta);
+            ans[Beta.size()]++;
+        }
+        return ;
+    }
+    for(auto v=tree.Header_Table.rbegin();v!=tree.Header_Table.rend();v++){
+        alpha.push_back(v->item);
+        res.push_back(alpha);
+        ans[alpha.size()]++;
+        //生成条件模式基
+        fpnode*p=v->head,*q;
+        db.clear();
+        while (p!=nullptr)
+        {
+            q=p->fa;
+            tmp.clear();
+            while (q->fa!=nullptr)
+            {
+                tmp.push_back(q->item);
+                q=q->fa;
+            }
+            if(tmp.size()){
+                tmp.push_back(p->count);    //在最后添加出现次数
+                db.push_back(tmp);
+            }
+            p=p->next;
+        }
+        //db生成条件模式树
+        if(db.empty()) {
+            alpha.pop_back();
+            continue;
+        }
+        FP_Tree newtree=build();
+        FPgrowth(newtree);
+        alpha.pop_back();
+    }
+}
+int main(){
+    path="./C++/retail.dat";
+    //读数据、建树
+    FP_Tree ft=first_construct();
+    //调用FP-Gowth算法进行挖掘 
+    FPgrowth(ft);
+    cout<<"total: "<<res.size()<<" items\n";
+    for(auto v:ans)  printf("%d-frequent item: %d\n",v.first,v.second);
+    //写入文件
+    ofstream outf("./C++/fpgrowth.txt");
+    outf<<"total: "<<res.size()<<" items\n";
+    for(auto v:ans)  outf<<v.first<<"-frequent item:"<<v.second<<'\n';
+    for(auto tp:res){
+        outf<<"{ ";
+        for(int i:tp) outf<<i<<' ';
+        outf<<"}\n";
+    }
+    outf.close();
+    return 0;
+}
